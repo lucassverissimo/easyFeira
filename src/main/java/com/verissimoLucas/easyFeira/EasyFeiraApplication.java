58ce6b95aa0ec8770bccaee70a386217f90d0ec6
Lucas Veríssimo <lucass.verissimo@gmail.com>
2020-10-24 19:33:25 -0300

    Início do projeto

---
+++ src/main/java/com/verissimoLucas/easyFeira/EasyFeiraApplication.java
@@ -0,0 +1,13 @@
+package com.verissimoLucas.easyFeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verissimoLucas/easyFeira/EasyFeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verissimoLucas.easyFeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
